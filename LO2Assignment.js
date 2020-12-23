d15b9a95fa994e09a534e3281ae313e1dc016d52
Mark Anthony Novida <76100738+Markiee018@users.noreply.github.com>
2020-12-23 14:00:56 +0800

    LO2Assignment.js

--- LO2Assignment.js
+++ LO2Assignment.js
@@ -150,17 +150,3 @@ for (i = 1; i < links.length; i++){
 }
     document.write("<br>The Links is now looped");
 
-// Break & Continue
-
-for (i = 0; i < 10; i++){
-
-    if(i === 5 || i === 3){
-        continue;
-    }
-    console.log(i);
-
-    if(i === 7){
-        break;
-    }
-}
-console.log('I have broken out of the loop');
